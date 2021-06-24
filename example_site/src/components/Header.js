6ab15c211c9386639de7c4b67ed01628d12eca83
Jenny Sun <jennysunnjm@gmail.com>
2021-06-24 11:17:29 -0400

    PR Review 2 - Directly used function call to get proper orientation value instead of setting it to a variable - Added new lines at end of files - Included more in-depth comments

--- example_site/src/components/Header.js
+++ example_site/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import "./Header.css"
 
 /**
- * Creates a Header reusable component
+ * Renders a title and subtitle, intended to be used as a Header
  * 
  * @param {string} title Title of header
  * @param {string} subtitle Subtitle of header 
