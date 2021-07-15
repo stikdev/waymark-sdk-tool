987d5c3ac838050c5bfaf857b3d9f916aa5a691e
Jenny Sun <jennysunnjm@gmail.com>
2021-07-15 13:49:47 -0400

    Update: Template Browser Collections Page Category Selection

--- example_site/src/components/Collections.js
+++ example_site/src/components/Collections.js
@@ -12,23 +12,26 @@ function Template({ template }) {
   const { openEditor } = useAppContext();
 
   return (
-    <li className="template">
-      <button title={template.id} onClick={() => openEditor({ template })}>
-        <img
-          className="thumbnail"
-          src={template.thumbnailImageURL}
-          alt={`${template.name} thumbnail`}
-        />
-        <div>{template.name}</div>
+    <div>
+      <button className="template-button" title={template.id} onClick={() => openEditor({ template })}>
+        <div className='image-container'>
+            <img
+              className="thumbnail"
+              src={template.thumbnailImageURL}
+              alt={`${template.name} thumbnail`}
+            />
+        </div>
+        <div>Name: {template.name}</div>
+        <div>Aspect Ratio: {template.aspectRatio}</div>
+        <div>Duration: {template.duration}</div>
       </button>
-    </li>
+    </div>
   );
 }
 
-function Collection({
+function CollectionNames({
   collection,
   setSelectedCollection,
-  expand,
   templateFilter,
 }) {
   const { waymarkInstance, addTemplates } = useAppContext();
@@ -37,12 +40,52 @@ function Collection({
     templateFilter
   );
 
+  const onClick = (collection) => {
+    setSelectedCollection(collection);
+    console.log("Clicked");
+  };
+
+  const shouldShowRefetchWithFilterButton = !_.isEqual(
+    templateFilter,
+    currentTemplateFilter
+  );
+
+  return (
+    <div className="category-name">
+      <button className="collection-name" onClick={() => onClick(collection)}>
+            {collection.name}
+      </button>
+
+      {shouldShowRefetchWithFilterButton ? (
+        <button
+          onClick={() => setCurrentTemplateFilter(templateFilter)}
+          className="collection-filter-refetch"
+        >
+          Refetch with new filters
+        </button>
+      ) : null}
+    </div>
+  );
+}
+
+function CollectionTemplates({
+  collection,
+  expand,
+  templateFilter,
+}) {
+  const { waymarkInstance, addTemplates } = useAppContext();
+
+  // const [currentTemplateFilter, setCurrentTemplateFilter] = useState(
+  //   templateFilter
+  // );
+
+  // useQuery to get the right templates given the collection name
   const { isLoading, isError, isSuccess, data: templates, error } = useQuery(
-    ["templates", collection.id, currentTemplateFilter],
+    ["templates", collection, /* currentTemplateFilter*/],
     () =>
       waymarkInstance.getTemplatesForCollection(
         collection.id,
-        currentTemplateFilter
+        /*currentTemplateFilter*/
       ),
     { enabled: !!waymarkInstance && expand }
   );
@@ -53,52 +96,29 @@ function Collection({
     }
   }, [templates, addTemplates]);
 
-  const onClick = (collection) => {
-    setSelectedCollection(collection);
-  };
-
-  const shouldShowRefetchWithFilterButton = !_.isEqual(
-    templateFilter,
-    currentTemplateFilter
-  );
-
   return (
-    <li className="collection">
-      <button className="collection-name" onClick={() => onClick(collection)}>
-        {collection.name} ({collection.id})
-      </button>
-      {shouldShowRefetchWithFilterButton && (
-        <button
-          onClick={() => setCurrentTemplateFilter(templateFilter)}
-          className="collection-filter-refetch"
-        >
-          Refetch with new filters
-        </button>
-      )}
-      {expand && (
-        <>
-          {isLoading && <div className="loading">Loading...</div>}
-          {isError && (
-            <div className="error">Error loading collection. {error}</div>
-          )}
-          <ul className="collection">
-            {isSuccess &&
-              templates &&
-              templates.map((template) => (
-                <Template key={template.id} template={template} />
-              ))}
-          </ul>
-        </>
-      )}
-    </li>
+      <>
+        {expand ?  (
+          <>
+            {isLoading ? (<div className="loading">Loading...</div>) : null}
+            {isError ? (
+              <div className="error">Error loading collection. {error}</div>
+            ) : null}
+            <div className="template-grids">
+              {(isSuccess && templates) ? (
+                templates.map((template) => (
+                  <Template key={template.id} template={template} />
+                ))) : null}
+            </div>
+          </>
+        ) : null}
+      </>
   );
 }
 
 export default function Collections() {
   const [selectedCollection, setSelectedCollection] = useState(null);
-  const [templateFilter, setTemplateFilter] = useState(() => ({
-    aspectRatio: "16:9",
-  }));
+  // const [templateFilter, setTemplateFilter] = useState(() => ({}));
   const { waymarkInstance } = useAppContext();
 
   const { isLoading, isError, isSuccess, data: collections, error } = useQuery(
@@ -117,27 +137,37 @@ export default function Collections() {
         filtered by length and/or aspect ratio. Show any or all of
         them any way that you like."
       />
-      {isLoading && <div className="loading">Loading...</div>}
-      {isError && <div className="error">Error: {error}</div>}
-      {isSuccess && (
-        <>
+      {isLoading ? (
+        <div className="loading">Loading...</div>
+      ) : null}
+
+      {isError ? (
+        <div className="error">Error: {error}</div>
+      ) : null}
+
+      {isSuccess ? (
+        <>{/*
           <h3>Template Filters</h3>
-          <JsonEditor value={templateFilter} onChange={setTemplateFilter} />
-          <ul className="collections">
-            {collections.map((collection) => (
-              <Collection
-                key={collection.id}
-                collection={collection}
+        <JsonEditor value={templateFilter} onChange={setTemplateFilter} /> */}
+          <div className="browser-columns">
+            <div className="categories">
+              <h2>Categories</h2>
+              {collections.map((collection) => (
+                <CollectionNames
+                collection ={collection}
                 setSelectedCollection={setSelectedCollection}
-                expand={Boolean(
-                  selectedCollection && selectedCollection.id === collection.id
-                )}
-                templateFilter={templateFilter}
+                />
+              ))}
+            </div>
+            <div className="templates">
+              <CollectionTemplates
+                collection ={selectedCollection}
+                expand={selectedCollection != null}
               />
-            ))}
-          </ul>
+            </div>
+          </div>
         </>
-      )}
+      ) : null}
     </div>
   );
 }
